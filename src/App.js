160e22539929a802a30524f15267a4e00ca535ab
Artyom <art-sav2002@yandex.ru>
2026-08-07 14:40:47 +0700

    First class component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import Counter from './components/Counter';
+import ClassCounter from './components/ClassCounter';
 
 function App() {
   const [value, setValue] = React.useState('Text');
 
   return (
     <div className="App">
-      <Counter/>
-      <Counter/>
-      <Counter/>
-      <Counter/>
+      <ClassCounter/>
     </div>
   );
 }
