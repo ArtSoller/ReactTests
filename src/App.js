d46102dd9613bab003eee037a23d7b5940a1f02f
Artyom <art-sav2002@yandex.ru>
2026-08-07 20:36:04 +0700

    Create UI, css-modules and prototype of form

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import ClassCounter from './components/ClassCounter';
 import '../src/styles/App.css';
 import PostItem from './components/PostItem';
 import PostList from './components/PostList';
+import MyButton from './components/UI/button/MyButton';
+import MyInput from './components/UI/input/MyInput';
 
 function App() {
   const [posts, setPosts] = useState([
@@ -11,17 +13,15 @@ function App() {
     { id: 2, title: 'JavaScript 2', body: 'Description' },
     { id: 3, title: 'JavaScript 3', body: 'Description' }
   ]);
-  const [posts2, setPosts2] = useState([
-    { id: 1, title: 'Python', body: 'Description' },
-    { id: 2, title: 'Python 2', body: 'Description' },
-    { id: 3, title: 'Python 3', body: 'Description' }
-  ]);
-  // const [value, setValue] = useState('Text');
 
   return (
     <div className="App">
-      <PostList posts={posts} title="Посты про JS"/>
-      <PostList posts={posts2} title="Посты про Python"/>
+      <form>
+        <MyInput type="text" placeholder="Название поста" />
+        <MyInput type="text" placeholder="Описание поста" />
+        <MyButton disabled>Создать пост</MyButton>
+      </form>
+      <PostList posts={posts} title="Посты про JS" />
     </div>
   );
 }
