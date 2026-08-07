21e33989c3abacea3d1d9ecc4910808a4dbf4470
Artyom <art-sav2002@yandex.ru>
2026-08-07 14:56:31 +0700

    Styles + CSS

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React from 'react';
 import Counter from './components/Counter';
 import ClassCounter from './components/ClassCounter';
+import '../src/styles/App.css';
+import PostItem from './components/PostItem';
 
 function App() {
   const [value, setValue] = React.useState('Text');
 
   return (
     <div className="App">
-      <ClassCounter/>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
+      <PostItem/>
+
     </div>
   );
 }
