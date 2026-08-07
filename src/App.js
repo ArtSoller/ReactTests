f6bdf84d66d9f863131faa65997e2191919dfecd
Artyom <art-sav2002@yandex.ru>
2026-08-07 12:41:27 +0700

    First functional component

--- src/App.js
+++ src/App.js
@@ -1,28 +1,15 @@
-import react from 'react';
+import React from 'react';
+import Counter from './components/Counter';
 
 function App() {
-  const [likes, setLikes] = react.useState(5);
-  const [value, setValue] = react.useState('Text');
-
-  function increment() {
-    setLikes(likes + 1);
-  }
-
-  function decrement() {
-    setLikes(likes - 1);
-  }
+  const [value, setValue] = React.useState('Text');
 
   return (
     <div className="App">
-      <h1>Likes: {likes}</h1>
-      <h1>Value: {value}</h1>
-      <input 
-        type="text"
-        value={value}
-        onChange={event => setValue(event.target.value)}
-      ></input>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
+      <Counter/>
+      <Counter/>
+      <Counter/>
+      <Counter/>
     </div>
   );
 }
