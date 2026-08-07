b8950639e8cb61d7358f8ec8606c15ebe2fa4ead
Artyom <art-sav2002@yandex.ru>
2026-08-07 12:07:18 +0700

    Controlled input

--- src/App.js
+++ src/App.js
@@ -1,9 +1,28 @@
 import react from 'react';
 
 function App() {
+  const [likes, setLikes] = react.useState(5);
+  const [value, setValue] = react.useState('Text');
+
+  function increment() {
+    setLikes(likes + 1);
+  }
+
+  function decrement() {
+    setLikes(likes - 1);
+  }
+
   return (
     <div className="App">
-      
+      <h1>Likes: {likes}</h1>
+      <h1>Value: {value}</h1>
+      <input 
+        type="text"
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      ></input>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<div>Hello, World!</div>);
+root.render(<App />);
 
 // ReactDOM.render(<div>Hello, World!</div>, document.getElementById('root'));
